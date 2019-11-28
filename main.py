6045f391fe2a413f1b7a82877b9b2be876402bf1
balu <babichart@gmail.com>
2019-11-28 17:39:46 +0200

    11

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 
 
 def nitro_salt(meat_mass):
+    if meat_mass==1500:
+        print(z)
+    # print (z)
     try:
         salt_mass=int(int(meat_mass)/100)
     except:
+        # print (z)
         salt_mass=0
     return salt_mass
+def sugar(mm):
+    try:
+        sm=int(int(mm)/200)
+    except:
+        sm=0
+    return sm
--- tests/tests.py
+++ tests/tests.py
@@ -6,19 +6,30 @@ from main import *
 
 
 class TestNitrosalt(unittest.TestCase):
-    def test_nitro_salt_returns_mass(self):
+    def test_nitro_salt_returns_mass1(self):
+        print (1111)
         self.assertEqual(nitro_salt(1000),10)
         self.assertEqual(nitro_salt(1500),15)
 
-    def test_nitro_salt_returns_int(self):
-        self.assertIsInstance(nitro_salt(1000), int)
+    def test_nitro_salt_returns_int2(self):
+        print (2)
+        self.assertIsInstance(nitro_salt(1000.2), int)
+        # self.assertIsInstance(nitro_salt('ggkjg'), int)
 
-    def test_nitro_salt_receives_str_returns_int(self):
-        self.assertEqual(nitro_salt('1000'),10)
+    def test_nitro_salt_receives_str_returns_int3(self):
+        print (3)
+        self.assertEqual(nitro_salt('1000m'),10)
 
-    def test_nitro_salt_receives_alpha_returns_zero(self):
+    def test_nitro_salt_receives_alpha_returns_zero4(self):
+        print (4)
         self.assertEqual(nitro_salt('sldvg'),0)
 
+
+class TestSugar(unittest.TestCase):
+    def test_sugar_mass5(self):
+        print (5)
+        self.assertEqual(sugar(2000),10)
+        self.assertEqual(sugar(2000),20)
 
 if __name__ == '__main__':
     unittest.main()
